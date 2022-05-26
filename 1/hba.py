0fd6d8632adfbc86850f9b6ce513b6b6a0d4b69e
Lindemann-S <lisovin1889@gmail.com>
2022-05-26 17:31:53 +0300

    pep8 bugs fixed

--- 1/hba.py
+++ 1/hba.py
@@ -6,33 +6,36 @@ def hamming(seq1, seq2):
             result += 1
     return result
 
-def phash(str):
+
+def phash(string):
     p = 53
     result = 0
     p_pow = 1
-    for letter in str:
+    for letter in string:
         result += (ord(letter) - ord('a') + 1) * p_pow
         p_pow *= p
     return result
 
-def rabin_karp(sub_str, str):
+
+def rabin_karp(sub_str, string):
     result = 0
-    str_len = len(str)
+    str_len = len(string)
     sub_str_len = len(sub_str)
     sub_str_hash = phash(sub_str)
     for i in range(str_len - sub_str_len + 1):
-        str_hash = phash(str[i:i+sub_str_len])
-        if str_hash == sub_str_hash and str[i:i+sub_str_len] == sub_str:
+        str_hash = phash(string[i:i+sub_str_len])
+        if str_hash == sub_str_hash and string[i:i+sub_str_len] == sub_str:
             result += 1
     return result
 
+
 def kmers(seq, k):
-    dict = {}
+    dictionary = {}
     for i in range(len(seq) - k + 1):
         sub_str = seq[i:i+k]
-        dict[sub_str] = rabin_karp(sub_str, seq)
-    #print(dict)
-    return dict
+        dictionary[sub_str] = rabin_karp(sub_str, seq)
+    # print(dict)
+    return dictionary
 
 
 def distance1(seq1, seq2, k=2):
@@ -46,34 +49,35 @@ def distance1(seq1, seq2, k=2):
             kmers_seq2[item] = abs(kmers_seq2[item] - kmers_seq1[item])
     for item in kmers_seq2:
         result += kmers_seq2[item]
-    #print(result)
+    # print(result)
     return result
         
-
 
 def hba1(path, distance):
     file = open(path)
     lines = file.readlines()
     max_index = len(lines)
-    dict = {}
+    dictionary = {}
     if distance == hamming:
         for line1, index1 in zip(lines[:max_index - 1], range(max_index - 1)):
             for line2, index2 in zip(lines[index1 + 1:], range(index1 + 1, max_index)):
-                dict[index1 * 10 + index2] = hamming(line1, line2)
+                dictionary[index1 * 10 + index2] = hamming(line1, line2)
 
     if distance == distance1:
         for line1, index1 in zip(lines[:max_index - 1], range(max_index - 1)):
             for line2, index2 in zip(lines[index1 + 1:], range(index1 + 1, max_index)):
-                dict[index1 * 10 + index2] = distance1(line1, line2)
+                dictionary[index1 * 10 + index2] = distance1(line1, line2)
 
-    min_key = min(dict, key=dict.get)
+    min_key = min(dictionary, key=dictionary.get)
     print((min_key // 10, min_key % 10))
     
-                
-                
+
+def main():
+    hba1("./HBA1.txt", hamming)
+    hba1("./HBA1.txt", distance1)
+    # kmers("abracadabra", 2)
+    # distance1("abracadabra", "abracadabra")
 
 
-hba1("./HBA1.txt", hamming)
-hba1("./HBA1.txt", distance1)
-#kmers("abracadabra", 2)
-#distance1("abracadabra", "abracadabra")
+if __name__ == '__main__':
+    main()
--- 1/main.py
+++ 1/main.py
@@ -1,22 +1,20 @@
-
-
-
 def shape(m):
     assert m, "map must be not empty"
     return len(m), len(m[0])
 
-def printer(ch, curr, max):
-    if curr == max - 1:
+
+def printer(ch, curr, max_index):
+    if curr == max_index - 1:
         print(ch)
     else:
         print(ch, end='')
 
 
 def print_map(m, pos):
-    hight = len(m)
+    height = len(m)
     length = len(m[0])
-    assert pos[0] <= hight - 1 and pos[1] <= length - 1, "Indiana Jones left the world"
-    for row in range(hight):
+    assert pos[0] <= height - 1 and pos[1] <= length - 1, "Indiana Jones left the world"
+    for row in range(height):
         for col in range(length):
             if (row, col) == pos:
                 printer('@', col, length)
@@ -28,10 +26,10 @@ def print_map(m, pos):
 
 def neighbours(m, pos):
     result = []
-    hight = len(m)
+    height = len(m)
     length = len(m[0])
-    assert pos[0] < hight and pos[1] < length, "off-map position"
-    if pos[0] < hight - 1 and m[pos[0] + 1][pos[1]]:
+    assert pos[0] < height and pos[1] < length, "off-map position"
+    if pos[0] < height - 1 and m[pos[0] + 1][pos[1]]:
         result.append((pos[0] + 1, pos[1]))
     if pos[0] > 0 and m[pos[0] - 1][pos[1]]:
         result.append((pos[0] - 1, pos[1]))
@@ -39,15 +37,16 @@ def neighbours(m, pos):
         result.append((pos[0], pos[1] + 1))
     if pos[1] > 0 and m[pos[0]][pos[1] - 1]:
         result.append((pos[0], pos[1] - 1))
-    #print(result)
+    # print(result)
     return result
+
 
 def neighbours_without_labeled(m, dm, pos):
     result = []
-    hight = len(m)
+    height = len(m)
     length = len(m[0])
-    assert pos[0] < hight and pos[1] < length, "off-map position"
-    if pos[0] < hight - 1 and m[pos[0] + 1][pos[1]] and not dm[pos[0] + 1][pos[1]]:
+    assert pos[0] < height and pos[1] < length, "off-map position"
+    if pos[0] < height - 1 and m[pos[0] + 1][pos[1]] and not dm[pos[0] + 1][pos[1]]:
         result.append((pos[0] + 1, pos[1]))
 
     if pos[0] > 0 and m[pos[0] - 1][pos[1]] and not dm[pos[0] - 1][pos[1]]:
@@ -59,29 +58,29 @@ def neighbours_without_labeled(m, dm, pos):
     if pos[1] > 0 and m[pos[0]][pos[1] - 1] and not dm[pos[0]][pos[1] - 1]:
         result.append((pos[0], pos[1] - 1))
 
-    #print(result)
+    # print(result)
     return result
 
 
 def find_next_pos(dm, pos):
-    hight = len(dm)
+    height = len(dm)
     length = len(dm[0])
     current = dm[pos[0]][pos[1]]
-    if pos[0] < hight - 1 and dm[pos[0] + 1][pos[1]] + 1 == current:
-        return (pos[0] + 1, pos[1])
+    if pos[0] < height - 1 and dm[pos[0] + 1][pos[1]] + 1 == current:
+        return pos[0] + 1, pos[1]
 
     if pos[0] > 0 and dm[pos[0] - 1][pos[1]] + 1 == current:
-        return (pos[0] - 1, pos[1])
+        return pos[0] - 1, pos[1]
 
     if pos[1] < length - 1 and dm[pos[0]][pos[1] + 1] + 1 == current:
-        return (pos[0], pos[1] + 1)
+        return pos[0], pos[1] + 1
 
     if pos[1] > 0 and dm[pos[0]][pos[1] - 1] + 1 == current:
-        return (pos[0], pos[1] - 1)
+        return pos[0], pos[1] - 1
 
 
 def create_route(dm, start, finish):
-    result= []
+    result = []
     pos = finish
     while pos != start:
         result.append(pos)
@@ -89,43 +88,46 @@ def create_route(dm, start, finish):
     result.append(start)
     return result[::-1]
 
+
 def find_route(m, initial):
     map_size = shape(m)
-    distance_map = [[0 for j in range(map_size[1])] for i in range(map_size[0])]
+    distance_map = [[0] * map_size[0] for _ in range(map_size[0])]
     label = 1
     distance_map[initial[0]][initial[1]] = label
-    neighbours = neighbours_without_labeled(m, distance_map, initial)
-    while len(neighbours) != 0:
+    current_neighbours = neighbours_without_labeled(m, distance_map, initial)
+    while len(current_neighbours) != 0:
         label += 1
-        next = []
-        for neighbour in neighbours:
+        next_neighbours = []
+        for neighbour in current_neighbours:
             distance_map[neighbour[0]][neighbour[1]] = label
-            if neighbour[0] == 0 or neighbour[0] == map_size[0] - 1 or neighbour[1] == 0 or neighbour[1] == map_size[1] - 1:
+            if neighbour[0] == 0 or neighbour[0] == map_size[0] - 1 or neighbour[1] == 0 \
+                    or neighbour[1] == map_size[1] - 1:
                 route = create_route(distance_map, initial, neighbour)
-                #print(route)
+                # print(route)
                 return route
-            next.extend(neighbours_without_labeled(m, distance_map, neighbour))
-        neighbours = next
-    
+            next_neighbours.extend(neighbours_without_labeled(m, distance_map, neighbour))
+        current_neighbours = next_neighbours
+
+
 def escape(m, initial):
     route = find_route(m, initial)
     for element in route:
         print_map(m, element)
         print()
-    
 
 
-m = [[False, False, False, False], 
-     [False, True, False, True],
-     [False, True, True, False],
-     [False, False, True, False],
-     [False, True, True, False],
-     [False, True, False, True]]
+def main():
+    m = [[False, False, False, False],
+         [False, True, False, True],
+         [False, True, True, False],
+         [False, False, True, False],
+         [False, True, True, False],
+         [False, True, False, True]]
+    # print_map(m, (2, 1))
+    # neighbours(m, (2, 1))
+    # find_route(m, (1, 1))
+    escape(m, (1, 1))
 
-#print_map(m, (2, 1))
 
-#neighbours(m, (2, 1))
-
-#find_route(m, (1, 1))
-
-escape(m, (1, 1))
+if __name__ == '__main__':
+    main()
